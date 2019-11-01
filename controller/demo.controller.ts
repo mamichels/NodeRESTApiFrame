ceaba6d95c92cba3319e49138ba9959d365aa14a
mamichels <info@marcelmichels.de>
2019-11-01 18:06:29 +0100

    Rename demo routes

--- controller/demo.controller.ts
+++ controller/demo.controller.ts
@@ -12,31 +12,31 @@ export class DemoController implements ApiController {
     }
 
     public defineRoutes(router: Router) {
-        router.get('/votes', (req, res) => {
+        router.get('/demo', (req, res) => {
             this.demoDomain.getAll().then((models => {
                 return res.send(models);
             }));
         });
         
-        router.get('/votes/:id', (req, res) => {
+        router.get('/demo/:id', (req, res) => {
             this.demoDomain.getById(req.params.id).then((model => {
                 return res.send(model);
             }));
         });
         
-        router.post('/votes', (req, res) => {
+        router.post('/demo', (req, res) => {
             this.demoDomain.add(req.body).then(() => {
                 return res.send(true);
             });
         });
         
-        router.put('/votes/:id', (req, res) => {
+        router.put('/demo/:id', (req, res) => {
             this.demoDomain.edit(req.body).then(() => {
                 return res.send(true);
             });
         });
         
-        router.delete('/votes/:id', (req, res) => {
+        router.delete('/demo/:id', (req, res) => {
             this.demoDomain.delete(req.params.id).then(() => {
                 return res.send(true);
             });

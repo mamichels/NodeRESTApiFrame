f3b6dc98d46b65b5df985351762d7fcad1880bd0
mamichels <info@marcelmichels.de>
2019-11-01 18:07:28 +0100

    Rename demo routes

--- util/database.service.ts
+++ util/database.service.ts
@@ -3,7 +3,7 @@ import mongodb, { Db } from 'mongodb';
 export let db: Db;
 
 const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://roomVoteHandler:61nZAfzvzSGJu5CJ@roomvote-lymox.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "con string";
 
 MongoClient.connect(uri, { useNewUrlParser: true }).then( client => {
     db = client.db();
